b111183629210ce89e8657263ebf92aceca5e60d
Jaime Soffer <jaime.soffer@gmail.com>
2014-07-02 04:40:32 -0500

    fixing 'invalid-name'

--- data/tilerender.py
+++ data/tilerender.py
@@ -9,33 +9,44 @@ from . import pytmx
 class Renderer(object):
     """
     This object renders tile maps from Tiled
+
+    'filename' is a TMX map specification file
+    'tilemap' is a collection of tiles, all of the same size
+    'tilemap.width' and 'tilemap.height' are the number of tiles on the map
+
     """
+
     def __init__(self, filename):
-        tm = pytmx.load_pygame(filename, pixelalpha=True)
-        self.size = tm.width * tm.tilewidth, tm.height * tm.tileheight
-        self.tmx_data = tm
+        tilemap = pytmx.load_pygame(filename, pixelalpha=True)
+        self.size = (
+            tilemap.width * tilemap.tilewidth,
+            tilemap.height * tilemap.tileheight)
+        print(filename, tilemap.width, tilemap.height, tilemap.tilewidth,
+              tilemap.tileheight)
+        self.tmx_data = tilemap
 
     def render(self, surface):
 
-        tw = self.tmx_data.tilewidth
-        th = self.tmx_data.tileheight
-        gt = self.tmx_data.getTileImageByGid
+        tile_width = self.tmx_data.tilewidth
+        tile_height = self.tmx_data.tileheight
+        get_tile = self.tmx_data.getTileImageByGid
 
         if self.tmx_data.background_color:
             surface.fill(self.tmx_data.background_color)
 
         for layer in self.tmx_data.visibleLayers:
             if isinstance(layer, pytmx.TiledLayer):
-                for x, y, gid in layer:
-                    tile = gt(gid)
+                for tile_x, tile_y, gid in layer:
+                    tile = get_tile(gid)
                     if tile:
-                        surface.blit(tile, (x * tw, y * th))
+                        surface.blit(
+                            tile, (tile_x * tile_width, tile_y * tile_height))
 
             elif isinstance(layer, pytmx.TiledObjectGroup):
                 pass
 
             elif isinstance(layer, pytmx.TiledImageLayer):
-                image = gt(layer.gid)
+                image = get_tile(layer.gid)
                 if image:
                     surface.blit(image, (0, 0))
 
